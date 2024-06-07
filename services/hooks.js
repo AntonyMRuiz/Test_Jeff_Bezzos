c7c3b331834ca99c596783d6bc1e97ce76342278
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-06-07 17:53:12 -0500

    Update hooks

--- services/hooks.js
+++ services/hooks.js
@@ -3,33 +3,58 @@ import { URL_USERS } from "./route.js";
 
 (() => {
 
+    /* Obtenemos el ID usuario logeado, que debe esta en el LocalStorage */
     const userId = localStorage.getItem("userId");
+
+    /* Verificamos que ese Id, si pertenezca a un usuario */
     const userData = read(`${URL_USERS}/${userId}`)
 
+    /* Si no pertenece a ningun usuario, evitar el acceso a ruta privadas */
     if (userData) {
         alert("NO ESTAS LOGUEADO");
-        window.location.href = "../index.html"
-    }
+        /* Lo redireccionamos al login */
+        window.location.href = "login.html"
+    } else {
 
-    const path = window.location.pathname
-    const file = path.substring(path.lastIndexOf("/")+1);
-    /* ("/view/register.html").lastIndexOf("/") */
-    /* ("/view/register.html").substring(2) */
+        /* Es la ruta, sin la ruta raiz, de donde se esta ejecutando */
+        const path = window.location.pathname;
 
-    const routeAdmin = ["admin.html", "register.html"]
-    const routeVisitor = ["visitor.html"]
+        /* Es el nombre del archivo en donde se esta mirando */
+        const file = path.substring(path.lastIndexOf("/") + 1);
 
-    switch (userData["role"]) {
-        case "admin":
-                
-            break;
+        /* Metodos de los string */
+        /* ("/view/register.html").lastIndexOf("/") */
+        /* ("/view/register.html").substring(2) */
 
-        case "visitor":
+        /* Rutas privadas para el admin */
+        const routeAdmin = ["admin.html", "register.html"]
+        /* Rutas privadas para el visitante */
+        const routeVisitor = ["visitor.html"]
 
-            break;
+        /* Dependiendo al role, se verifica si tiene acceso o no a la ruta donde esta */
+        switch (userData["role"]) {
+            case "admin":
+                if (!routeAdmin.includes(file)) {
+                    alert("NO tienes permiso")
+                    /* Lo redireccionamos al home */
+                    window.location.href = "../index.html"
+                }
+                break;
 
-        default:
-            break;
+            case "visitor":
+                if (!routeVisitor.includes(file)) {
+                    alert("NO tienes permiso")
+                    /* Lo redireccionamos al home */
+                    window.location.href = "../index.html"
+                }
+                break;
+
+            default:
+                alert("Permisos invalidos")
+                /* Lo redireccinamos al home */
+                window.location.href = "../index.html"
+                break;
+        }
     }
 
 }
